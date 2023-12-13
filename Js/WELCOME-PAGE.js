91e5a92b3003d8df3a8355950b6b89cbfbbb8882
MatteoOrlando <mat.orlando88@gmail.com>
2023-12-13 13:28:54 +0100

    Welcome
    Html e Css sono ok, il lato js da definire

---
+++ Js/WELCOME-PAGE.js
@@ -0,0 +1,30 @@
+
+//  const proceed = function (){
+//    let proceedUrl = "inserire url";
+//    window.location.href =  proceedUrl
+
+//  }
+
+
+// const nextPage = function () {
+//     const proceed = document.getElementsByClassName("proceed");
+//     proceed.addEventListener("click", function () {
+//     window.location.href = "inserire link pagina benchmark";
+//     });
+//   };
+//   nextPage();
+
+// const checkbox = 
+
+function proceed(){
+
+    let checkBox = document.getElementsByClassName('checkBox');
+let checked = true
+    if(checkBox.checked)
+    proceed.addEventListener("click", )
+    {window.location.href = "inserire link pagina benchmark";
+    } else { 
+        alert("please, click on checkbox!")
+    } proceed()
+}
+proceed()
